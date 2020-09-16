bae172948fd63e371f266af53fc1ecc0ff654772
Your Name <you@example.com>
2020-09-16 11:31:28 +0200

    prg mié 16 sep 2020 11:31:28 CEST

---
+++ static/src/index.js
@@ -0,0 +1,8 @@
+import './index.scss';
+
+let container = document.getElementById('root');
+let element = document.createElement('h1');
+let text = document.createTextNode('adessso si ,va ddddbenissssssiomo');
+
+element.appendChild(text);
+container.appendChild(element);
